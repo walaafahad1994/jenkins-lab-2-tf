d0c1bf820925873ec180286488a8047839fbae48
Bryan Dollery <bryan.dollery@gmail.com>
2020-08-11 10:10:05 +0300

    ports

--- lab/main.tf
+++ lab/main.tf
@@ -134,9 +134,9 @@ resource "aws_security_group" "webserver" {
   }
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
